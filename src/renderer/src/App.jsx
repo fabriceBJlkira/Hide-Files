e38ad5534a93ce389e1823bdad2a6e6ebc926e69
Fabrice <fabricej035@gmail.com>
2024-11-03 10:49:29 +0100

    update app.jsx

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <AuthContextProvider>
       <RouterProvider router={Router} />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, accusantium reprehenderit temporibus eum officia ut obcaecati soluta? Impedit doloremque animi dignissimos, optio molestias quis esse non alias, mollitia earum quas.</p>
     </AuthContextProvider>
   )
 }
